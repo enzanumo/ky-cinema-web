a882022dcb515ddf7656eb342b75930a1145a9fe
Enzan_umo <enzanumo@outlook.com>
2022-12-07 21:29:27 +0800

    完善router，增加注释

--- internal/routers/router.go
+++ internal/routers/router.go
@@ -20,9 +20,8 @@ func NewRouter() *gin.Engine {
 	corsConfig.AddAllowHeaders("Authorization")
 	e.Use(cors.New(corsConfig))
 
-	// 按需注册 docs、静态资源、LocalOSS 路由
+	// 注册 静态资源 路由
 	{
-		registerDocs(e)
 		registerStatick(e)
 	}
 
@@ -44,11 +43,16 @@ func NewRouter() *gin.Engine {
 		// 获取用户基本信息
 		noAuthApi.GET("/user/profile", api.GetUserProfile)
 
+		// 获取电影信息
 		r.GET("/movies", api.Stub)
 
+		// 获取电影详情
 		r.GET("/movies/:id", api.Stub)
+
+		// 获取电影排期
 		r.GET("/movies/:id/schedule", api.Stub)
 
+		// 查询场次余票
 		r.POST("/seats", api.Stub)
 	}
 
@@ -56,36 +60,45 @@ func NewRouter() *gin.Engine {
 	authApi := r.Group("/").Use(middleware.JWT())
 	adminApi := r.Group("/").Use(middleware.JWT()).Use(middleware.Admin())
 	{
-
 		// 获取当前用户信息
 		authApi.GET("/user/info", api.GetUserInfo)
-
 		// 绑定用户手机号
 		{
 			authApi.POST("/user/phone", api.BindUserPhone)
 		}
-
 		// 修改密码
 		authApi.POST("/user/password", api.ChangeUserPassword)
-
 		// 修改昵称
 		authApi.POST("/user/nickname", api.ChangeNickname)
-
+		// 下单
 		authApi.POST("/orders/create", api.Stub)
+		// 支付
 		authApi.POST("/orders/pay", api.Stub)
+		// 获取我的订单
 		authApi.GET("/orders", api.Stub)
+		// 获取我的订单详情
 		authApi.GET("/orders/details/:id", api.Stub)
-
+		// 获取我的信息
 		authApi.GET("/user/info")
 
-		// 管理·禁言/解封用户
+		// 管理
+		// 新增影片
 		adminApi.POST("/movies", api.Stub)
+
+		// 获取所有场次
 		adminApi.GET("/schedule", api.Stub)
+
+		// 新增场次
 		adminApi.POST("/schedule", api.Stub)
+
+		// 新增场次优惠
 		adminApi.POST("/schedule/promotion", api.Stub)
 
+		// 查询用户信息
 		adminApi.GET("/user", api.Stub)
+		// 添加用户
 		adminApi.POST("/user", api.Stub)
+		// 修改用户
 		adminApi.POST("/user/:id", api.Stub)
 	}
 
